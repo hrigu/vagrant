09a9adeafe4d1d056175e70f2ee61664f86f86c8
hrigu <chmuehlethaler@gmail.com>
2012-09-17 09:36:12 +0200

    apache2: planik.config file

--- vms/precise64_ruby193/modules/apache2/manifests/init.pp
+++ vms/precise64_ruby193/modules/apache2/manifests/init.pp
@@ -1,23 +1,30 @@
 class apache2 {
-  exec { 'apt-get update':
-      command => '/usr/bin/apt-get update'
-  }
+    exec { 'apt-get update':
+        command => '/usr/bin/apt-get update'
+    }
 
-  package { "apache2":
-      ensure => present,
-      before => File["/etc/apache2/conf"]                       #ensures the package is installed before our custom config file is copied in.
-  }
+    package { "apache2":
+        ensure => present,
+        before => File["/etc/apache2/conf"]                         #ensures the package is installed before our custom config file is copied in.
+    }
 
-  service { "apache2":
-      ensure    => running,
-      require   => Package["apache2"],
-      subscribe => File["/etc/apache2/conf"]                    #puppet notifies the service when config changes happen
-  }
+    service { "apache2":
+        ensure    => running,
+        require   => Package["apache2"],
+        subscribe => File["/etc/apache2/conf"]                      #puppet notifies the service when config changes happen
+    }
 
-    file { "/etc/apache2/conf":
-      source  => "puppet:///modules/apache2/apache2.conf",      #interessanterweise nicht ../apaches2/files/apache2.conf
-      mode    => 644,
-      owner   => root,
-      group   => root
+    file {
+        "/etc/apache2/conf":
+            source  => "puppet:///modules/apache2/apache2.conf",    #interessanterweise nicht ../apaches2/files/apache2.conf
+            mode    => 644,
+            owner   => root,
+            group   => root;
+        "/etc/apache2/sites-enabled/planik.conf":
+            source  => "puppet:///modules/apache2/planik.conf",
+            owner   => root,
+            group   => root,
+            notify  => Service["apache2"],
+            require => Package["apache2"],
     }
 }
